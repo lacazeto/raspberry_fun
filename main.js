de3fc0644fcefc50abc5feffb8b3b0abf2bb136c
lacazeto <lucas.cazeto@gmail.com>
2018-10-20 15:09:19 +0200

    minor

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ IMU.getValue((err, data) => {
     return
   }
 
-  sense.showMessage(data.temperature.toFixed(0), [255, 0, 0])
+  sense.showMessage(data.temperature.toFixed(0), 0.2, [255, 0, 0])
 })
